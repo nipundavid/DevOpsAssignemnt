b9eb77e946e3ac094d41e6bbd412e8e4a310370f
nipundavid <nipun.david@nagarro.com>
2020-01-27 20:15:00 +0530

    update path

--- WebApplication4/obj/Debug/netcoreapp3.1/WebApplication4.AssemblyInfo.cs
+++ WebApplication4/obj/Debug/netcoreapp3.1/WebApplication4.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("1598c778-191a-4379-adad-8d632a891554")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
